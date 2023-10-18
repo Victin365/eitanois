d3bb5a81427f7cb7896bc6b1beb813a001abd291
Victin365 <138122744+Victin365@users.noreply.github.com>
2023-10-18 11:25:52 -0300

    Add files via upload

---
+++ eitanois/script.js
@@ -0,0 +1,23 @@
+function calcular(){
+
+var valorEtanol = parseFloat(etanol.value);
+var valorGasolina = parseFloat(gasolina.value);
+
+var custo = (valorEtanol/valorGasolina);
+
+if (custo > 0.7) {
+    alert("Use Gasolina");
+}
+
+if (custo < 0.7){
+    alert("Use Etanol");
+}
+
+if (custo == 0.7){
+    alert("qualquer um");
+}
+
+
+
+
+}
